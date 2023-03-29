a7f3a565cb0e59116eab0ed3c904764729348623
Bruno Miranda <brunomiranda@outlook.com.ar>
2023-03-29 20:25:51 -0300

    Ejercicio 2 de subprogramas

--- Guia4/src/ejercicios/ej2.java
+++ Guia4/src/ejercicios/ej2.java
@@ -3,9 +3,30 @@ package ejercicios;
 
 import java.util.Scanner;
 public class ej2 {
+     public static void datosPersona() {
+        Scanner leer = new Scanner(System.in);
+        int n, edad;
+        String nombre, opcion;
+        boolean mayor;
+        do {
+
+            System.out.println("Ingrese el nombre de la persona: ");
+            nombre = leer.next();
+
+            System.out.println("Ingrese la edad de " + nombre + ": ");
+            edad = leer.nextInt();
+
+            mayor = (edad >= 18);
+            System.out.println(nombre + " tiene " + edad + " años y es " + (mayor ? "mayor" : "menor") + " de edad.");
+
+            System.out.println("¿Desea ingresar más personas? (Sí/No)");
+            opcion = leer.next();
+        } while (!opcion.equalsIgnoreCase("No"));
+    }
 
     
     public static void main(String[] args) {
+        datosPersona();
         
        
     }
